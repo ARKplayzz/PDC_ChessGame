35ebae1d8fd0cf97bf25b30d9b0bb1d6fe1d22ab
Andrew <Andrew@kennedys.nz>
2025-06-14 22:46:25 +1200

    Added Database Tests

--- test/pdc_chessgame/DatabaseTest.java
+++ test/pdc_chessgame/DatabaseTest.java
@@ -80,19 +80,48 @@ public class DatabaseTest {
     }
     
     @Test
-    public void testGetSavesForUser()
+    public void testAlterPlayer() 
     {
-        System.out.println("Getting saves for user");
-        
-        
+        boolean updated = instance.alterPlayer("TEST__USER_9@3467%4321", 42, 3, 2);
+        assertTrue(updated);
+        assertEquals(42, instance.getElo("TEST__USER_9@3467%4321"));
+        assertEquals(3, instance.getGamesWon("TEST__USER_9@3467%4321"));
+        assertEquals(2, instance.getGamesLost("TEST__USER_9@3467%4321"));
     }
-    
+
     @Test
-    public void testDeletePlayer()
+    public void testDeleteGames() 
     {
-        System.out.println("Deleting player");
-        boolean expected = true;
-        boolean actual = instance.deletePlayers("TEST__USER_9@3467%4321");
-        assertEquals(expected, actual);
+        instance.insertGame("TEST_SAVE_191930506432", "TEST__USER_9@3467%4321", "fakename alias", "Fake path", 58, 57);
+        boolean deleted = instance.deleteGames("TEST_SAVE_191930506432");
+        assertTrue(deleted);
+        assertFalse(instance.gameExists("TEST_SAVE_191930506432"));
+    }
+
+    @Test
+    public void testGameExists() 
+    {
+        instance.insertGame("TEST_SAVE_191930506432", "TEST__USER_9@3467%4321", "fakename alias", "Fake path", 58, 57);
+        assertTrue(instance.gameExists("TEST_SAVE_191930506432"));
+        instance.deleteGames("TEST_SAVE_191930506432");
+        assertFalse(instance.gameExists("TEST_SAVE_191930506432"));
+    }
+
+    @Test
+    public void testGetSavesForUser() 
+    {
+        instance.insertGame("TEST_SAVE_191930506432", "TEST__USER_9@3467%4321", "fakename alias", "Fake path", 58, 57);
+        var saves = instance.getSavesForUser("TEST__USER_9@3467%4321");
+        assertFalse(saves.isEmpty());
+        assertEquals("TEST_SAVE_191930506432", saves.get(0).saveFile);
+    }
+
+    @Test
+    public void testGetLeaderboard() 
+    {
+        String leaderboard = instance.getLeaderboard().toString();
+        assertNotNull(leaderboard);
+        assertTrue(leaderboard.contains("Username"));
+        assertTrue(leaderboard.length() > 0);
     }
 }
